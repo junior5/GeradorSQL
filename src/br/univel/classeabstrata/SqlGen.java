9091f06533d3d12a917c46bb7ae16cfb478bdd9a
Junior <saoj07@gmail.com>
2016-04-20 22:12:43 -0300

    Adicionado os PreparedStatements

--- src/br/univel/classeabstrata/SqlGen.java
+++ src/br/univel/classeabstrata/SqlGen.java
@@ -1,5 +1,16 @@
 package br.univel.classeabstrata;
 
-public class SqlGen {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+public abstract class SqlGen {
+	// DDL
+	protected abstract String getCreateTable(Connection con, Object obj);
+	protected abstract String getDropTable(Connection con, Object obj);
+	// DML
+	protected abstract PreparedStatement getSqlInsert(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlSelectAll(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlSelectById(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlUpdateById(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlDeleteById(Connection con, Object obj);
 }
