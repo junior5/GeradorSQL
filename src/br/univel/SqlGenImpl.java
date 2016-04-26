b4f72e8f890797749250920dc8183f1897593454
Junior <saoj07@gmail.com>
2016-04-26 19:28:52 -0300

    Testando códigos na classe SqlGenImpl.

--- src/br/univel/SqlGenImpl.java
+++ src/br/univel/SqlGenImpl.java
@@ -14,106 +14,7 @@ public class SqlGenImpl extends SqlGen {
 
 	@Override
 	protected String getCreateTable(Connection con, Object obj) {
-		Class<? extends Object> cl = obj.getClass();
-		try {
-
-			StringBuilder sb = new StringBuilder(); 
-			{
-				String nomeTabela;
-				if (cl.isAnnotationPresent(Tabela.class)) {
-
-					Tabela anotacaoTabela = cl.getAnnotation(Tabela.class);
-					nomeTabela = anotacaoTabela.value();
-
-				} else {
-					nomeTabela = cl.getSimpleName().toUpperCase();
-
-				}
-				sb.append("CREATE TABLE ").append(nomeTabela).append(" (");
-			}
-
-			Field[] atributos = cl.getDeclaredFields();
-			{
-				for (int i = 0; i < atributos.length; i++) {
-
-					Field field = atributos[i];
-
-					String nomeColuna;
-					String tipoColuna;
-
-					if (field.isAnnotationPresent((Class<? extends Annotation>) Coluna.class)) {
-						Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
-
-						if (anotacaoColuna.nome().isEmpty()) {
-							nomeColuna = field.getName().toUpperCase();
-						} else {
-							nomeColuna = anotacaoColuna.nome();
-						}
-
-					} else {
-						nomeColuna = field.getName().toUpperCase();
-					}
-
-					Class<?> tipoParametro = field.getType();
-
-					if (tipoParametro.equals(String.class)) {
-						tipoColuna = "VARCHAR(100)";
-
-					} else if (tipoParametro.equals(int.class)) {
-						tipoColuna = "INT";
-
-					} else {
-						tipoColuna = "DESCONHECIDO";
-					}
-
-					if (i > 0) {
-						sb.append(",");
-					}
-
-					sb.append("\n\t").append(nomeColuna).append(' ').append(tipoColuna);
-				}
-			}
-
-			{
-
-				sb.append(",\n\tPRIMARY KEY( ");
-
-				for (int i = 0, achou = 0; i < atributos.length; i++) {
-
-					Field field = atributos[i];
-
-					if (field.isAnnotationPresent(Coluna.class)) {
-
-						Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
-
-						if (anotacaoColuna.pk()) {
-
-							if (achou > 0) {
-								sb.append(", ");
-							}
-
-							if (anotacaoColuna.nome().isEmpty()) {
-								sb.append(field.getName().toUpperCase());
-							} else {
-								sb.append(anotacaoColuna.nome());
-							}
-
-							achou++;
-						}
-
-					}
-				}
-
-				sb.append(" )");
-			}
-
-			sb.append("\n);");
-
-			return sb.toString();
-
-		} catch (SecurityException e) {
-			throw new RuntimeException(e);
-		}
+		return null;
 	}
 
 	@Override
@@ -124,9 +25,104 @@ public class SqlGenImpl extends SqlGen {
 
 	@Override
 	protected PreparedStatement getSqlInsert(Connection con, Object obj) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Class<? extends Object> cl = obj.getClass();
+
+		StringBuilder sb = new StringBuilder();
+
+		// Declaração da tabela.
+		{
+			String nomeTabela;
+			if (cl.isAnnotationPresent(Tabela.class)) {
+
+				Tabela anotacaoTabela = cl.getAnnotation(Tabela.class);
+				nomeTabela = anotacaoTabela.value();
+
+			} else {
+				nomeTabela = cl.getSimpleName().toUpperCase();
+
+			}
+			sb.append("INSERT INTO ").append(nomeTabela).append(" (");
+		}
+
+		Field[] atributos = cl.getDeclaredFields();
+
+		// Declaração das colunas
+		{
+			for (int i = 0; i < atributos.length; i++) {
+
+				Field field = atributos[i];
+
+				String nomeColuna;
+
+				if (field.isAnnotationPresent(Coluna.class)) {
+					Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
+
+					if (anotacaoColuna.nome().isEmpty()) {
+						nomeColuna = field.getName().toUpperCase();
+					} else {
+						nomeColuna = anotacaoColuna.nome();
+					}
+
+				} else {
+					nomeColuna = field.getName().toUpperCase();
+				}
+
+				if (i > 0) {
+					sb.append(", ");
+				}
+
+				sb.append(nomeColuna);
+			}
+		}
+
+		sb.append(") VALUES (");
+
+		for (int i = 0; i < atributos.length; i++) {
+			if (i > 0) {
+				sb.append(", ");
+			}
+			sb.append('?');
+		}
+		sb.append(')');
+
+		String strSql = sb.toString();
+		System.out.println(strSql);
+
+		PreparedStatement ps = null;
+		
+		try {
+			ps = con.prepareStatement(strSql);
+
+			for (int i = 0; i < atributos.length; i++) {
+				Field field = atributos[i];
+
+				// importante não esquecer
+				field.setAccessible(true);
+
+				if (field.getType().equals(int.class)) {
+					ps.setInt(i + 1, field.getInt(obj));
+
+				} else if (field.getType().equals(String.class)) {
+					ps.setString(i + 1, String.valueOf(field.get(obj)));
+
+				} else {
+					throw new RuntimeException("Tipo não suportado, falta implementar.");
+
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return ps;
 	}
+	
 
 	@Override
 	protected PreparedStatement getSqlSelectAll(Connection con, Object obj) {
