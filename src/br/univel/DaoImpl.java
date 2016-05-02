082ad1230893c31fcb71dc85d4d91f13a22e87b7
Junior <saoj07@gmail.com>
2016-05-01 23:09:20 -0300

    Modificado o método  salvar em DaoImpl.

--- src/br/univel/DaoImpl.java
+++ src/br/univel/DaoImpl.java
@@ -2,6 +2,7 @@ package br.univel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -29,8 +30,30 @@ public class DaoImpl implements Dao<Object, Object> {
 	
 	@Override
 	public void salvar(Object t) {
-		// TODO Auto-generated method stub
 
+		PreparedStatement insert = getImp().getSqlInsert(getImp().getCon(), t);
+
+		System.out.println("INCLUINDO REGISTRO");
+
+		Cliente cliente = (Cliente) t;
+
+		try {
+			insert.setInt(1, cliente.getId());
+			insert.setString(2, cliente.getNome());
+			insert.setString(3, cliente.getEndereco());
+			insert.setString(4, cliente.getTelefone());
+			insert.setInt(5, cliente.getEstadoCivil().ordinal());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+
+		try {
+			insert.executeUpdate();
+			System.out.println("REGISTRO INCLUIDO!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 
