a4fabf529167ab7715246ce273e7fc24522e6b1b
Junior <saoj07@gmail.com>
2016-05-03 19:40:33 -0300

    Adicionado método cabeçalho na classe Main

--- src/br/univel/Main.java
+++ src/br/univel/Main.java
@@ -14,6 +14,7 @@ public class Main extends DaoImpl {
 		
 		Cliente c1 = new Cliente(1, "Junior", "Rua: Carijós", "9988-6760", EstadoCivil.SOLTEIRO);
 
+		cabecalho("APAGANDO TABELA:");
 		apagarTabela(c1);
 
 		criarTabela(c1);
@@ -24,7 +25,7 @@ public class Main extends DaoImpl {
 
 		salvar(c2);
 
-		Cliente c3 = new Cliente(3, "Teste", "Avenida Brasil", "s/n", EstadoCivil.VIUVO);
+		Cliente c3 = new Cliente(3, "Teste", "Avenida Brasil", "0000000", EstadoCivil.VIUVO);
 
 		salvar(c3);
 
@@ -32,10 +33,10 @@ public class Main extends DaoImpl {
 
 		buscar(c1);
 
-		c3.setNome("NOME 3");
-		c3.setEndereco("ENDERECO 3");
-		c3.setTelefone("TELEFONE 3");
-		c3.setEstadoCivil(EstadoCivil.VIUVO);
+		c3.setNome("NOME ATUALIZADO");
+		c3.setEndereco("ENDERECO ATUALIZADO");
+		c3.setTelefone("TELEFONE ATUALIZADO");
+		c3.setEstadoCivil(EstadoCivil.SOLTEIRO);
 
 		atualizar(c3);
 
@@ -44,6 +45,13 @@ public class Main extends DaoImpl {
 		listarTodos(new Cliente());	
 		
 	}
+	
+	private void cabecalho(String string) {
+		System.out.println("\n----------------------------------------------");
+		System.out.println(string);
+		System.out.println("----------------------------------------------");
+	}
+
 	
 	public static void main(String[] args) {
 		new  Main();
